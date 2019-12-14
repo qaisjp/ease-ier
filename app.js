4bc869d6ad5a880c6684cbc0c5920f9db77ca3de
Qais Patankar <qaisjp@gmail.com>
2019-12-14 18:37:49 +0000

    Fix return key and visuals

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 function main() {
-    const newHTML = `
-<div class="form-group required">
-	<label for="password" class="control-label">Password</label>
-    <input
-        type="password"
-        name="password"
-        id="password"
-        class="form-control input-lg" autocomplete="off"
-        autocapitalize="off" required="required" tabindex="1"
-    >
-</div>`
     const help = document.querySelector(".help-block")
-    help.innerHTML = newHTML
+    const username_group = help.parentElement
+    help.remove()
 
-    const form = help.closest("form")
+    const pw = document.createElement("div")
+    pw.classList.add("form-group", "required")
+    pw.innerHTML = `
+        <label for="password" class="control-label">Password</label>
+        <input
+            type="password"
+            name="password"
+            id="password"
+            class="form-control input-lg" autocomplete="off"
+            autocapitalize="off" required="required" tabindex="1"
+        >`
+
+    username_group.after(pw)
+
+    const form = pw.closest("form")
     form.setAttribute("action", "/cosign.cgi")
 
     console.log("Old EASE has fixed this page.")
 }
+
 window.addEventListener("load", main);
